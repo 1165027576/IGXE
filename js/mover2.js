737cdb685cf9edc6a597ca39d1799726c11182fd
Zhanghao <1165027576@qq.com>
2019-02-25 09:10:56 +0800

    差购物车

--- js/mover2.js
+++ js/mover2.js
@@ -269,12 +269,3 @@ function parabola03(domObj,startPoint,endPoint,timeLong,openDirection,func){
 }
 
 
-function $(str){
-	if(str.charAt(0)=="#"){
-		return document.getElementById(str.substring(1));
-	}else if(str.charAt(0)=="."){
-		return document.getElementsByClassName(str.substring(1));
-	}else{
-		return document.getElementsByTagName(str);
-	}
-}
